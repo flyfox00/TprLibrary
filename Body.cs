3ec0147aa79852c2f5523b3c2197f96550bceb6b
chen Jung Min <crm00@ms4.hinet.net>
2022-05-15 10:18:40 +0800

    加入專案檔案。

---
+++ Body.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace TprLibrary
+{
+    public class BodyRec : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private decimal? bw = null;
+        public decimal Bw
+        {
+            get => (decimal)bw;
+            set
+            {
+                if (value != bw)
+                {
+                    bw = value;
+                    OnPropertyChanged(nameof(Bw));
+                }
+            }
+        }
+
+        private decimal? bh = null;
+        public decimal Bh
+        {
+            get => (decimal)bh;
+            set
+            {
+                if (value != bh)
+                {
+                    bh = value;
+                    OnPropertyChanged(nameof(Bh));
+                }
+            }
+        }
+
+        public event EventHandler BodyBuild;
+        public void OnBodyBuild(BodyEventArgs e)
+        {
+            BodyBuild?.Invoke(this, e);
+        }
+
+        public void Build()
+        {
+            BodyEventArgs bodyEventArgs = new BodyEventArgs(this.Bw, this.Bh);
+            OnBodyBuild(bodyEventArgs);
+        }
+    }
+    public class BodyEventArgs : EventArgs
+    {
+        public decimal Bw { get; private set; }
+        public decimal Bh { get; private set; }
+        public BodyEventArgs(decimal bw, decimal bh)
+        {
+            this.Bw = bw;
+            this.Bh = bh;
+        }
+    }
+    public static class BodyRecExt
+    {
+        public static int Lastest(this BodyRec body)
+        {
+            // 最近一 次
+            int uid = 0;
+            return uid;
+        }
+
+        public static bool Update(this BodyRec body, int uid)
+        {
+            // 更新紀錄
+            return true;
+        }
+
+        public static bool Remove(this BodyRec body, int uid)
+        {
+            // 移除紀錄
+            return true;
+        }
+    }
+}
---
+++ TPR.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace TprLibrary
+{
+    public class TprRec : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private decimal? temperature = null;
+        public decimal Temperature
+        {
+            get => (decimal)temperature;
+            set
+            {
+                if (value != temperature)
+                {
+                    temperature = value;
+                    OnPropertyChanged(nameof(Temperature));
+                }
+            }
+        }
+
+        private int pulse = 0;
+        public int Pulse
+        {
+            get => pulse;
+            set
+            {
+                if (value != pulse)
+                {
+                    pulse = value;
+                    OnPropertyChanged(nameof(Pulse));
+                }
+            }
+        }
+
+        private int respiratory = 0;
+        public int Respiratory
+        {
+            get => respiratory;
+            set
+            {
+                if (value != respiratory)
+                {
+                    respiratory = value;
+                    OnPropertyChanged(nameof(Respiratory));
+                }
+            }
+        }
+
+        private int sbp = 0;
+        public int Sbp
+        {
+            get => sbp;
+            set
+            {
+                if (value != sbp)
+                {
+                    sbp = value;
+                    OnPropertyChanged(nameof(Sbp));
+                }
+            }
+        }
+
+        private int dbp = 0;
+        public int Dbp
+        {
+            get => dbp;
+            set
+            {
+                if (value != dbp)
+                {
+                    dbp = value;
+                    OnPropertyChanged(nameof(Dbp));
+                }
+            }
+        }
+    }
+    public class TprEventArgs : EventArgs
+    {
+        public decimal Temperature { get; private set; }
+        public int Pulse { get; private set; }
+        public int Respiratory { get; private set; }
+        public int Sbp { get; private set; }
+        public int Dbp { get; private set; }
+        public TprEventArgs(decimal temperature, int pulse,
+           int respiratory, int sbp, int dbp)
+        {
+            this.Temperature = temperature;
+            this.Pulse = pulse;
+            this.Respiratory = respiratory;
+            this.Sbp = sbp;
+            this.Dbp = dbp;
+        }
+    }
+
+}
